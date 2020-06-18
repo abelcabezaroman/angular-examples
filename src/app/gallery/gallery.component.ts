36417aff541ba4a90f274cd8e3c5407b5930f6ae
Abel Cabeza Román <contacto@abelcabezaroman.com>
2020-06-18 13:29:32 +0200

    new excercises

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
@@ -7,11 +7,48 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  @Input() gallery;
+
+  gallery = [
+    {
+      imgUrl: 'https://picsum.photos/500/350?random=1',
+      caption: 'So beautiful!',
+      createdBy: 'Abel'
+    }, {
+      imgUrl: 'https://picsum.photos/500/350?random=2',
+      caption: 'So Ugly!',
+      createdBy: 'Abel'
+    }, {
+      imgUrl: 'https://picsum.photos/500/350?random=3',
+      caption: 'No problem',
+      createdBy: 'Abel'
+    }, {
+      imgUrl: 'https://picsum.photos/500/350?random=4',
+      caption: 'Lets go',
+      createdBy: 'Abel'
+    }, {
+      imgUrl: 'https://picsum.photos/500/350?random=5',
+      caption: 'So cute!',
+      createdBy: 'Abel'
+    }, {
+      imgUrl: 'https://picsum.photos/500/350?random=6',
+      caption: 'Look at this!',
+      createdBy: 'Abel'
+    }
+  ]
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  newItemEmitter($event) {
+    // $event = {
+    //   imgUrl: 'https://picsum.photos/500/350?random=6',
+    //   caption: 'Look at this!',
+    //   createdBy: 'Abel'
+    // }
+    this.gallery.push($event);
   }
 
 }
